1d4be307257655eb9dd921e373ba15be1d81e6b7
Mihai Chifor <chifor.mihai21@gmail.com>
2019-08-15 00:31:01 +0300

    Add Podspec

--- ios/RCTWebViewBridge.h
+++ ios/RCTWebViewBridge.h
@@ -10,12 +10,8 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#if __has_include(<React/RCTView.h>)
 #import <React/RCTView.h>
-#else
-#import "RCTView.h"
-#endif
-    
+
 @class RCTWebViewBridge;
 
 /**
